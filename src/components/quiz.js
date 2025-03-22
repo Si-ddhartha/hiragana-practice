e602b192d94ef20a051ad6d88c875ce77796bcf1
Aniket <74449359+Si-ddhartha@users.noreply.github.com>
2025-03-22 11:49:32 +0530

    Added user input and performing comparison with actual romaji

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./quiz.css";
 
 import hiragana from "../assets/hiragana";
@@ -7,6 +7,10 @@ import hiragana from "../assets/hiragana";
 function Quiz({selectedOptions}){
     const [filteredKanas, setFilteredKanas] = useState([])
     const [currentKana, setCurrentKana] = useState(hiragana[0])
+    const [userInput, setUserInput] = useState("")
+    const [resultMessage, setResultMessage] = useState("")
+
+    const inputRef = useRef(null)
 
     useEffect(() => {   
         const filteredList = hiragana.filter((item) => 
@@ -20,6 +24,18 @@ function Quiz({selectedOptions}){
         if (filteredKanas.length > 0) {
             const randomKana = filteredKanas[Math.floor(Math.random() * filteredKanas.length)]
             setCurrentKana(randomKana)
+            setUserInput("")
+            setResultMessage("")
+            inputRef.current?.focus()
+        }
+    }
+
+    const handleCheck = () => {
+        if (userInput.toLowerCase() === currentKana.romaji.toLowerCase()){
+            setResultMessage("Correct")
+        }
+        else{
+            setResultMessage("Incorrect")
         }
     }
 
@@ -29,10 +45,27 @@ function Quiz({selectedOptions}){
                 <h1>{currentKana.kana}</h1>
             </div>
 
+            <div className="quiz-input">
+                <input
+                    ref={inputRef}
+                    name="user-input" 
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
+                    autoFocus
+                />
+            </div>
+
             <div className="quiz-buttons">
-                <button>Check</button>
+                <button onClick={handleCheck}>Check</button>
                 <button onClick={getRandomKana}>Next</button>
             </div>
+
+            <div 
+                    className="quiz-question-result" 
+                    style={{color: resultMessage === "Correct" ? "darkgreen" : "darkred"}}
+                >
+                    <p>{resultMessage}</p>
+                </div>
         </div>
     )
 }
